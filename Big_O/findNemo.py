5f46eef878dc302707782ca66ffd8ddfd237fc0a
Hardik Munjal <hardy96tech@gmail.com>
2022-02-26 15:47:44 +0000

    Time complexity with varying array sizes

--- Big_O/findNemo.py
+++ Big_O/findNemo.py
@@ -1,6 +1,22 @@
+from time import time
+
 small_arr = ["nemo"]
 
-medium_arr = [""]
+medium_arr = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank']
+
+large_arr = ["Random"]*1000000
+large_arr.append("nemo")
+
+def findNemo(arr):
+  start = time()
+
+  for i in range(0, len(arr)):
+    if arr[i] == "nemo":
+      print("FOUND NEMO!")
+  
+  end = time() - start
+  print(f'Total time taken to run the program: {"{:.2f}".format(end)} seconds')
+
 
 if __name__ == "__main__":
-  print(arr)
+  findNemo(large_arr)
